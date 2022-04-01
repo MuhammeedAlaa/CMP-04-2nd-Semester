d4c161bfbb9ad1297b1e67a53d30cb3ec864a723
Alaa <soupermado@gmail.com>
2022-04-01 18:20:57 +0200

    [BigData][Lab3][fix-problem1-comments]

--- BigData/Lab3/Naive Bayes Classifier.R
+++ BigData/Lab3/Naive Bayes Classifier.R
@@ -5,10 +5,10 @@ library("e1071")
 # 1. First of all, start by cleaning the workspace and setting the working directory.
 rm(list=ls())
 setwd("/Users/muhammadalaa/Desktop/CMP/CMP-04-2nd-Semester/BigData/Lab3/")
+
 # 2. Divide the data into two data frames
 sample <- read.table("nbtrain.csv", header=TRUE, sep=",")
-
-
+# age gender educ income
 
 # 3. Divide the data into two data frames
 traindata <- as.data.frame(sample[1:9000,])
@@ -18,18 +18,25 @@ testdata <- as.data.frame(sample[9001:nrow(sample),])
 
 # 4. Train a Naïve Bayes Classifier model with income as the target variable and all other variables as independent variables. Smooth the model with Laplace smoothing coefficient = 0.01
 model <- naiveBayes(income~.,traindata)
+# Laplace smoothing is a smoothing technique that handles the problem of zero probability in Naïve Bayes. Using Laplace smoothing
+
 # 5. display model
 model
+
 # 6. predict with test data
 results <- predict (model, testdata)
 
 # 7. Display a confusion matrix for the predict values of the test data versus the actual values
-table(testdata$income, results)
+table(results, testdata$income)
+# class 10=50K is over-represented in the taring test and class 2,3 have few samples in the training set (Imbalanced Data problem)
+# aprior probability is large for class 1.
 
 # 8 Display the accuracy of the model.
 print(paste0("Acc = %", sum(results == testdata$income)/nrow(testdata) * 100))
+# for imbalanced data accuracy is not enough to judge the model 
+#so we need to apply more evaluation criteria like(f1-score, precision, recall) to judge the model
 
-# 9. Display the overall 10-50K, 50-80K, GT 80K misclassification rates.
+# 9. Display the overall 10-50K, 50-80K, GT 80K mis-classification rates.
 
 print(paste0("misclassification rates (10-50K) = % ", sum(results != testdata$income & testdata$income == '10-50K') / sum(testdata$income == '10-50K') * 100))
 print(paste0("misclassification rates (50-80K) = % ", sum(results != testdata$income & testdata$income == '50-80K') / sum(testdata$income == '50-80K') * 100))
