8e0002a4fbe65e54e12a451f0c5fc0817cf4eae2
Alaa <soupermado@gmail.com>
2022-04-03 15:34:51 +0200

    [Robotics][Lab1][Problem2][Add:finalize]

--- Robotics/Lab 1/main.py
+++ Robotics/Lab 1/main.py
@@ -1,6 +1,7 @@
 from typing import Any
 import cv2 as cv
 import numpy as np
+from datetime import datetime
 
 
 class Robot:
@@ -46,6 +47,7 @@ class Sensor:
         self.sensor_angle = 250
         self.sensor_resolution = 2
         self.max_range = 1200 / 4
+        self.z = []
 
     def sense_destance(self, pose_x_test: int, pose_y_test: int, robot: Robot):
         return np.sqrt((pose_x_test - robot.x) ** 2 + (pose_y_test - robot.y) ** 2)
@@ -62,12 +64,19 @@ class Sensor:
         start = robot.theta - int(self.sensor_angle / 2)
         end = robot.theta + int(self.sensor_angle / 2)
         file.write('\n\t\t\tmeasurements\n rayX, rayY, raylength, RayAngle \n')
-        for angle in range(start, end, self.sensor_resolution):
+        for angle in range(start, end + 1, self.sensor_resolution):
             new_x, new_y = robot.x, robot.y
             while (self.check_ray_collision(test_map, new_x, new_y)) is False and self.sense_destance(new_x, new_y, robot) <= self.max_range:
                 new_x, new_y = self.move_ray(1, new_x, new_y, angle)
             cv.line(test_map, (robot.x, robot.y), (int(new_x), int(new_y)), 200, 2)
-            file.write(str(new_x) + ', ' + str(new_y) + ', ' + str(self.sense_destance(new_x, new_y, robot) * 4) + ', ' + str(angle) + '\n')            
+            destance = -1
+            if self.sense_destance(new_x, new_y, robot) >= self.max_range:
+                self.z.append(1200)
+                destance = 1200
+            else:
+                self.z.append(self.sense_destance(new_x, new_y, robot) * 4)
+                destance = self.z[-1]
+            file.write(str(new_x) + ', ' + str(new_y) + ', ' + str(destance) + ', ' + str(angle) + '\n')            
         return test_map
 
 class Problem1:
@@ -79,15 +88,59 @@ class Problem1:
         test_map_with_robot_and_rays = sensor.measurements_draw_rays(robot, test_map, file)
         test_map_with_robot_and_rays = robot.draw(test_map_with_robot_and_rays)
         file.close()
-        cv.imshow('map', test_map_with_robot_and_rays)
-        cv.waitKey(0)
+        _ = cv.imwrite('likelihood_field.png', test_map_with_robot_and_rays)
+
+
+class Proplem2():
+    def __init__(self, test_map: Any):
+        self.likelihood_field = cv.GaussianBlur(255 - test_map, (5, 5), 4)
+        self.propbability_map = np.zeros(shape=(len(test_map), len(test_map[0])))
+
+
+    def solve(self, test_map: Any, robot: Robot, sensor: Sensor):
+        _ = cv.imwrite('likelihood_field.png', self.likelihood_field)
+        for y in range(0, self.likelihood_field.shape[0]):
+            for x in range(0, self.likelihood_field.shape[1]):
+                for angle in range(0, 360):
+                    start = robot.theta - int(sensor.sensor_angle / 2)
+                    end = robot.theta + int(sensor.sensor_angle / 2)
+                    index = 0
+                    prop = 1
+                    for rayAngle in range(start, end + 1, sensor.sensor_resolution):
+                        end_x, end_y = sensor.move_ray(sensor.z[index] / 4, x, y, rayAngle + angle)
+                        if sensor.z[index] >= 1200:
+                            continue
+                        if (end_y >= len(self.likelihood_field) or end_x >= len(self.likelihood_field[0]) or end_y < 0 or end_x < 0) == False:
+                            prop *= self.likelihood_field[int(end_y)][int(end_x)]
+                        else:
+                            prop *= 0.01
+                        index += 1
+                    self.propbability_map[y][x] = max(self.propbability_map[y][x], prop)
+        maximum = max(map(max, self.propbability_map))
+        position = -1, -1
+        x = cv.imwrite('prob_map.png', self.propbability_map) 
+        if maximum > 0: 
+            self.propbability_map = [[(x / maximum) * 255 for x in y] for y in self.propbability_map]
+            for y in range(len(self.propbability_map)):
+                for x in range(len(self.propbability_map[0])):
+                    if self.propbability_map[y][x] == 255:
+                        position = x, y 
+        robot.x = position[0]
+        robot.y = position[1]
+        map_with_robot = robot.draw(test_map)
+        x = cv.imwrite('robot_pose.png', map_with_robot)
 
 
 if __name__ == "__main__":
+    now = datetime.now()
     test_map = cv.imread('Assignment_04_Grid_Map.png', cv.IMREAD_GRAYSCALE)
-    file = open("output.txt", "a")
+    file = open("output.txt", "w")
     robot = Robot()
     robot.x, robot.y, robot.theta =  370, 182, 180
     sensor = Sensor()
     problem1 = Problem1(file)
     problem1.solve(robot, test_map, sensor)
+    test_map = cv.imread('Assignment_04_Grid_Map.png', cv.IMREAD_GRAYSCALE)
+    problem2 = Proplem2(test_map)
+    problem2.solve(test_map, robot, sensor)
+    print(datetime.now() - now)
